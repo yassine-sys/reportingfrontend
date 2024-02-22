8ab7348b7f6ee6ca328e87af97b59fce76b53ef5
Taha Ayari <spamf369@gmail.com>
2024-02-22 12:54:36 +0100

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://172.16.54.7:8080/reportingback',
-  recApi: 'http://172.16.54.7:8080/rec',
-  chartApi: 'http://172.16.54.7:8080/RaftoolsReporting/rest',
+  apiUrl: 'http://10.156.35.98:9998/reportingback',
+  recApi: 'http://10.156.35.98:9998/rec',
+  chartApi: 'http://10.156.35.98:9998/RaftoolsReporting/rest',
   recaptchaSiteKey: '6LfMIGcpAAAAAH0ZGLLgnLu72CgNOIhHvkN3jMDi',
 };
